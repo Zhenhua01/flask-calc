c919a5189b6a916efcfbd0bdc17aa165b5c46746
Zhenhua <zhenhua1109@yahoo.com>
2022-07-27 12:36:34 -0700

    complete add function

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 # Put your app in here.
+
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+@app.get('/add')
+def add_nums():
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return str(operations.add(a, b))
